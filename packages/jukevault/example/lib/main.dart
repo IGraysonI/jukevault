3f1de84ec6bace64e8ae50a45c95e47c4f4f0915
Akzhan Ardaki <Akzhan.Ardaki@raimbek.com>
2024-04-11 21:13:08 +0500

    added example

---
+++ packages/jukevault/example/lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:jukevault/jukevault.dart';
+
+void main() => runApp(
+      const MaterialApp(
+        home: Songs(),
+      ),
+    );
+
+class Songs extends StatefulWidget {
+  const Songs({super.key});
+
+  @override
+  _SongsState createState() => _SongsState();
+}
+
+class _SongsState extends State<Songs> {
+  // Main method.
+  final JukeVault _jukeVault = JukeVault();
+
+  // Indicate if application has permission to the library.
+  bool _hasPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // (Optinal) Set logging level. By default will be set to 'WARN'.
+    //
+    // Log will appear on:
+    //  * XCode: Debug Console
+    //  * VsCode: Debug Console
+    //  * Android Studio: Debug and Logcat Console
+    LogConfig logConfig = LogConfig(logType: LogType.DEBUG);
+    _jukeVault.setLogConfig(logConfig);
+
+    // Check and request for permission.
+    checkAndRequestPermissions();
+  }
+
+  checkAndRequestPermissions({bool retry = false}) async {
+    // The param 'retryRequest' is false, by default.
+    _hasPermission = await _jukeVault.checkAndRequest(
+      retryRequest: retry,
+    );
+
+    // Only call update the UI if application has all required permissions.
+    _hasPermission ? setState(() {}) : null;
+  }
+
+  Widget noAccessToLibraryWidget() => Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.redAccent.withOpacity(0.5),
+        ),
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("Application doesn't have access to the library"),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => checkAndRequestPermissions(retry: true),
+              child: const Text("Allow"),
+            ),
+          ],
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("OnAudioQueryExample"),
+          elevation: 2,
+        ),
+        body: Center(
+          child: !_hasPermission
+              ? noAccessToLibraryWidget()
+              : FutureBuilder<List<SongModel>>(
+                  // Default values:
+                  future: _jukeVault.querySongs(
+                    sortType: null,
+                    orderType: OrderType.ASC_OR_SMALLER,
+                    uriType: UriType.EXTERNAL,
+                    ignoreCase: true,
+                  ),
+                  builder: (context, item) {
+                    // Display error, if any.
+                    if (item.hasError) {
+                      return Text(item.error.toString());
+                    }
+
+                    // Waiting content.
+                    if (item.data == null) {
+                      return const CircularProgressIndicator();
+                    }
+
+                    // 'Library' is empty.
+                    if (item.data!.isEmpty) return const Text("Nothing found!");
+
+                    // You can use [item.data!] direct or you can create a:
+                    // List<SongModel> songs = item.data!;
+                    return ListView.builder(
+                      itemCount: item.data!.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(item.data![index].title),
+                          subtitle: Text(item.data![index].artist ?? "No Artist"),
+                          trailing: const Icon(Icons.arrow_forward_rounded),
+                          // This Widget will query/load image.
+                          // You can use/create your own widget/method using [queryArtwork].
+                          leading: QueryArtworkWidget(
+                            controller: _jukeVault,
+                            id: item.data![index].id,
+                            type: ArtworkType.AUDIO,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+        ),
+      );
+}
--- packages/jukevault_android/lib/jukevault_android.dart
+++
@@ -1,8 +0,0 @@
-
-import 'jukevault_android_platform_interface.dart';
-
-class JukevaultAndroid {
-  Future<String?> getPlatformVersion() {
-    return JukevaultAndroidPlatform.instance.getPlatformVersion();
-  }
-}
